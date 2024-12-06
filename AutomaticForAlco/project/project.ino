221ea572cd6bf0b24f19b849dd56ded75466b93b
Alexander <49525149+AlexKeyMAD@users.noreply.github.com>
2024-12-07 00:02:15 +0500

    create

---
+++ AutomaticForAlco/project/project.ino
@@ -0,0 +1,42 @@
+#include <LiquidCrystal_I2C.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 2
+#define term_power 4
+
+LiquidCrystal_I2C LCD(0x27,20,4);
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature temp(&oneWire);
+
+void setup()
+ {
+  
+  LCD.init();
+  LCD.backlight();
+  LCD.setCursor(0,0);
+  LCD.print("Hello, world!");
+  delay(1000);
+
+  LCD.clear();
+  LCD.setCursor(0,0);
+  LCD.print("Temperature");
+
+  //Serial.begin(9600);
+  temp.begin();
+  pinMode(term_power, OUTPUT);
+
+  digitalWrite(term_power, HIGH);
+  temp.requestTemperatures();
+}
+
+void loop() {
+  
+  temp.requestTemperatures();
+  LCD.setCursor(12,0);
+
+  float t = temp.getTempCByIndex(0);
+  LCD.print(t);
+  delay(1000);
+
+}
