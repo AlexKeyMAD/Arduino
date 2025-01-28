09101f5e6d0ca848c8df5480f90750915e351895
Alexander <49525149+AlexKeyMAD@users.noreply.github.com>
2025-01-28 21:23:26 +0500

    update

--- AutomaticForAlco/project/project.ino
+++ AutomaticForAlco/project/project.ino
@@ -1,14 +1,14 @@
 #include <LiquidCrystal_I2C.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
+#include <GyverNTC.h>
 
-#define ONE_WIRE_BUS 2  //порт датчика температуры (данные) D2
-#define term_power 4    // порт датчика температуры (питание) D4
+#define but_Up 8 //Кнопка вверх D8
+#define but_Down 9 //Кнопка вверх D9
 
 LiquidCrystal_I2C LCD(0x27,20,4); //порты дисплея SDA – A4, SCL – A5
 
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature temp(&oneWire);
+GyverNTC ntc(A1, 9980, 3950, 25, 10000);
+
+float tempMemory = 0;
 
 void setup()
  {
@@ -21,23 +21,60 @@ void setup()
 
   LCD.clear();
   LCD.setCursor(0,0);
-  LCD.print("Temperature");
+  LCD.print("Temp:");
 
-  //Serial.begin(9600);
-  temp.begin();
-  pinMode(term_power, OUTPUT);
+  pinMode(but_Up, INPUT_PULLUP);
+  pinMode(but_Down, INPUT_PULLUP);
 
-  digitalWrite(term_power, HIGH);
-  temp.requestTemperatures();
 }
 
 void loop() {
   
-  temp.requestTemperatures();
-  LCD.setCursor(12,0);
+  LCD.setCursor(10,0);
 
-  float t = temp.getTempCByIndex(0);
+  float t = ntc.getTemp();
   LCD.print(t);
   delay(1000);
 
+  bool btUp = digitalRead(but_Up);
+
+  if (!btUp)
+  {    
+    delay(150);
+    Update_tempMemory(true, t);
+  }
+
+  bool btDw = digitalRead(but_Down);
+
+  if (!btDw)
+  {    
+    delay(150);    
+    Update_tempMemory(false, t);
+  }
+
+  CheckTemperature(t);
+}
+
+void Update_tempMemory(bool up, float t)
+{
+  if (tempMemory == 0) tempMemory = round(t * 10) / 10;
+
+  if (up) tempMemory += 0.1;
+  else tempMemory -= 0.1;
+
+  LCD.setCursor(10,1);
+  LCD.print(tempMemory);
+}
+
+void CheckTemperature(float t)
+{
+  if (tempMemory != 0 && tempMemory <= t)
+  {
+    LCD.setCursor(2,1);
+    LCD.print("*");
+  } else
+  {
+    LCD.setCursor(2,1);
+    LCD.print(" ");
+  }
 }
